bc944237637df91346d89a91e164ce3594877c9a
huanghaoxuan <huanghaoxuan1998@outlook.com>
2019-06-21 22:23:20 +0800

    初始化项目

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,36 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import axios from "axios";
+import Qs from "qs";
+import "@babel/polyfill";
+
+Vue.prototype.axios = axios;
+Vue.prototype.qs = Qs;
+axios.defaults.baseURL = "/api";
+Vue.use(Antd);
 
 Vue.config.productionTip = false;
+
+router.beforeEach((to, from, next) => {
+  //console.log(store.state.loginStatus);
+  if (store.state.loginStatus) {
+    next();
+  } else {
+    store.commit("isLogin", true);
+    next("/login");
+
+    let allowBack = true; //    给个默认值true
+    if (to.meta.allowBack !== undefined) {
+      allowBack = to.meta.allowBack;
+    }
+    if (!allowBack) {
+      history.pushState(null, "", location.href);
+    }
+  }
+});
 
 new Vue({
   router,
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    loginStatus: false
+  },
+  mutations: {
+    isLogin(state, status) {
+      state.loginStatus = status;
+    }
+  },
+  actions: {},
+  plugins: [createPersistedState({ storage: window.sessionStorage })]
 });

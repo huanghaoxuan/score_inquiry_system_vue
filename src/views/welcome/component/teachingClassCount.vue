9cab0ab7feaa0f01a3e33d0c55572932e2a3728a
huanghaoxuan <huanghaoxuan1998@outlook.com>
2019-07-31 16:07:52 +0800

    加入教师课程统计图像分析

---
+++ src/views/welcome/component/teachingClassCount.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <a-spin :spinning="spinning" size="large">
+      <v-chart :forceFit="true" :height="height" :data="data" :scale="scale">
+        <v-tooltip />
+        <v-axis />
+        <v-bar position="year*count" />
+      </v-chart>
+    </a-spin>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: null,
+      scale: null,
+      spinning: true,
+      height: 400
+    };
+  },
+  methods: {
+    getData() {
+      this.axios
+        .get("/analysis/teachingclass/" + this.$store.state.name, {
+          headers: {
+            Authorization: this.$store.state.token
+          }
+        })
+        .then(
+          function(res) {
+            const data = res.data.data;
+            const scale = [
+              {
+                dataKey: "count",
+                tickInterval: 20
+              }
+            ];
+            this.data = data;
+            this.scale = scale;
+            this.spinning = false;
+          }.bind(this)
+        )
+        .catch(
+          function(err) {
+            if (err.response) {
+              //console.log(err.response);
+              //控制台打印错误返回的内容
+              if (err.response.status == 403) {
+                //console.log(err.response);
+                this.$notification.error({
+                  message: "账号密码已过期，请重新登录！"
+                });
+                this.$router.push("/login");
+                //控制台打印错误返回的内容
+              }
+            }
+            //bind(this)可以不用
+          }.bind(this)
+        );
+    }
+  },
+  mounted() {
+    this.getData();
+  }
+};
+</script>
--- src/views/welcome/welcome.vue
+++ src/views/welcome/welcome.vue
@@ -1,21 +1,31 @@
 <template>
   <div style="background:#ECECEC; padding:30px;width:100%">
-    <a-card title="成绩分析">
+    <a-card title="成绩分析" v-if="permissions == 1">
       <score-chart></score-chart>
     </a-card>
     <br />
-    <a-card title="课程总计">
+    <a-card title="课程总计" v-if="permissions == 1">
       <score-count></score-count>
+    </a-card>
+    <a-card title="任课班总计" v-if="permissions != 1">
+      <teaching-class-count></teaching-class-count>
     </a-card>
   </div>
 </template>
 <script>
 import scoreChart from "./component/scoreChart";
 import scoreCount from "./component/scoreCount";
+import teachingClassCount from "./component/teachingClassCount";
 export default {
   components: {
     scoreChart,
-    scoreCount
+    scoreCount,
+    teachingClassCount
+  },
+  data() {
+    return {
+      permissions: this.$store.state.permissions
+    };
   }
 };
 </script>
